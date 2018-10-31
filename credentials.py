5b9697f8b94c77b046fded6ba6a70ab109c6c00b
Chris-karanja <chriswainaina0@gmail.com>
2018-10-31 13:31:01 +0300

    finding credentials method included

--- credentials.py
+++ credentials.py
@@ -15,3 +15,17 @@ class Credentials:
     def add_credentials(self):
         """ save new credentials to Accounts List """
         Credentials.Accounts.append(self)    
+
+
+@classmethod
+def find_credentials(cls, Network):
+        """
+        Method that takes in a network and returns a credential that matches that network.
+         Args:
+            network: platform to search for
+        Returns :
+            details of platform that matches network.
+        """
+        for saved_credentials in cls.Accounts:
+            if saved_credentials.platform == Network:
+                return saved_credentials
--- program_test.py
+++ program_test.py
@@ -13,7 +13,7 @@ class TestUser(unittest.TestCase):
 
     def tearDown(self):
         """Runs after every test case"""
-        User.Users = []
+        Credentials.Accounts = []
 
     def test_init(self):
         """Test whether object is initialized properly"""
@@ -22,16 +22,18 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.password, "password123")
 
 
-def test_add_user(self):
+    def test_add_user(self):
         """ test whether new user is added to Users list """
         self.new_user.add_user()
         self.assertEqual(len(User.Users), 1)
-def test_save_multiple_contact(self):
+
+def test_save_multiple_users(self):
             ''' test whether multiple users can be stored'''
             self.new_user.add_user()
             other_user= User("Cyborg","cyborg@gmail.com","password456") # new user
             other_user.add_user()
             self.assertEqual(len(User.Users),2)
+
 def test_find_user(self):
         ''' test whether user can be found by username and password'''
         self.new_user.add_user()
@@ -44,24 +46,41 @@ def test_find_user(self):
 class TestCredential(unittest.TestCase):
     """ Test class for defining test cases for credential behavior """
 
-    def setUp(self):
+def setUp(self):
         """runs before each credential test case"""
         self.new_credential = Credentials(
              "Dance", "Weshlysnipes", "password123")
 
-    def tearDown(self):
+def tearDown(self):
         """ Runs after each credential test case"""
         User.Accounts = []
 
-    def test_init(self):
+def test_init(self):
         """ Test whether credential objects are initialized properly """
         self.assertEqual(self.new_credential.platform, "Dance")
         self.assertEqual(self.new_credential.platform_username, "Weshlysnipes")
         self.assertEqual(self.new_credential.platform_password, "password123")
 
-    def test_add_credentials(self):
+def test_add_credentials(self):
         """ test whether new credential is added to Accounts list """
         self.new_credential.add_credentials()
         self.assertEqual(len(Credentials.Accounts), 1)
+
+def test_save_multiple_credentials(self):
+        """ test whether multiple users can be stored """
+        self.new_credential.add_credentials()
+        other_credential = Credentials("Fortnite", "Jojo", "password123")
+        other_credential.add_credentials()
+        self.assertEqual(len(Credentials.Accounts), 2)
+
+def test_find_credential(self):
+        """ test whether credential can be found by platform """
+        self.new_credential.add_credentials()
+        other_credential = Credentials("Fortnite", "Jojo", "password123")
+        other_credential.add_credentials()
+        retrieved_credential = Credential.find_credentials("Fortnite")
+        self.assertEqual(retrieved_credential.platform,
+                         other_credential.platform)
+
 if __name__ == '__main__':	
     unittest.main()	    
